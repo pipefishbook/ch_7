398b5116e8be0c59c278a7244498308f3ce3d741
Patrick Mulder <mulder.patrick@gmail.com>
2014-04-14 23:42:19 +0200

    fetch

---
+++ fetch/app/routers/movies.js
@@ -0,0 +1,45 @@
+var Backbone = require('backbone');
+var _ = require('underscore');
+
+// data
+var Movies = require('collections/movies');
+var movies = new Movies();
+var deferred = movies.fetch();
+
+// views
+var Layout = require('views/layout');
+
+var MoviesRouter = Backbone.Router.extend({
+
+  routes: {
+    'movies/:id': 'selectMovie',
+    '':           'showMain'
+  },
+
+  selectMovie: function(id) {
+    var that = this;
+    deferred.done(function() {
+      movies.resetSelected();
+      movies.selectByID(id);
+      that.layout.setDetails(that.movies.get(id));
+    });
+  },
+
+  showMain: function() {
+    movies.resetSelected();
+    this.layout.setChose();
+  },
+
+  initialize: function(options) {
+    this.movies = movies;
+    this.layout = Layout.getInstance({
+      el: '#movies', router: this
+    });
+    var that = this;
+    deferred.done(function(results) {
+      that.movies.reset(results);
+      that.layout.render();
+    });
+  }
+});
+module.exports = MoviesRouter;
